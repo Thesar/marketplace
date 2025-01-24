841d8cf24ebc25531d190c6589a07cfd0640a39f
Thesar <thesar.habibaj@gmail.com>
2025-01-24 06:12:07 +0100

    added javascript to the pages

---
+++ script.js
@@ -0,0 +1,11 @@
+function addToCart() {
+    alert('Item added to cart!');
+}
+
+function changeTextColor() {
+    document.getElementById('aboutText').style.color = 'red';
+}
+
+function resetTextColor() {
+    document.getElementById('aboutText').style.color = 'black';
+}
